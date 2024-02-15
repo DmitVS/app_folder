707d1b173c7fa0df6d2fad477e3f4181a66ba77f
Dmitry <dmitry@mail.com>
2024-02-15 21:53:29 +0300

    Созданы index и main

--- main/views.py
+++ main/views.py
@@ -7,15 +7,16 @@ from django.shortcuts import render
 
 def index(request):
     context={
-        'title' : 'Home',
-        'content' : 'Главная страница - Home',
-        'list' : ['first', 'second'],
-        'dict' : {'first' : 1},
-        'is_authenticated' : True
+        'title' : 'Home - Главная',
+        'content' : 'Магазин мебели HOME'
     }
-
-    return render(request, 'main/index.html', context)
+    return render(request, 'main/about.html', context)
 
 
 def about(request):
-    return HttpResponse('About page')
+    context={
+        'title' : 'Home - О нас',
+        'content' : 'О нас',
+        'text_on_page' : 'Текст большой текст. Текст большой текст. Текст большой текст. Текст большой текст. Текст большой текст. Текст большой текст. Текст большой текст. Текст большой текст.'
+    }
+    return render(request, 'main/about.html', context)
