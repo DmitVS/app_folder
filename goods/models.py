41552a1f00a04f1960cea087cf5b7ee4da0272e5
Dmitry <dmitry@email.com>
2024-02-19 19:56:41 +0300

    Fixtures для Goods

--- goods/models.py
+++ goods/models.py
@@ -9,14 +9,17 @@ class Categories(models.Model):
         verbose_name='Категория'
         verbose_name_plural='Категории'
 
+    def __str__(self):
+        return self.name
+
 
 class Products(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Наименование')
     slug = models.SlugField(max_length=200, unique=True, blank=True, null=True, verbose_name='URL')
     description = models.TextField(blank=True, null=True, verbose_name='Описание')
-    image = models.ImageField(upload_to='goods_mages', blank=True, null=True, verbose_name='Изображение')
+    image = models.ImageField(upload_to='goods_images', blank=True, null=True, verbose_name='Изображение')
     price = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, blank=True, null=True, verbose_name='Цена')
-    discount = models.DecimalField(default=0.00, max_digits=7, decimal_places=2, blank=True, null=True, verbose_name='Скидка')
+    discount = models.DecimalField(default=0.00, max_digits=5, decimal_places=2, blank=True, null=True, verbose_name='Скидка')
     quantity = models.PositiveIntegerField(default=0, blank=True, null=True, verbose_name='Количество')
     category = models.ForeignKey(to=Categories, on_delete=models.CASCADE, verbose_name='Категория')
 
@@ -24,3 +27,6 @@ class Products(models.Model):
         db_table='product'
         verbose_name='Продукт'
         verbose_name_plural='Продукты'
+
+    def __str__(self):
+        return f'{self.name} Количество - {self.quantity}'
--- main/views.py
+++ main/views.py
@@ -1,14 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from goods.models import Categories
 
 #def index(request):
 #    return HttpResponse('Home page')
 
 def index(request):
+
+    categories = Categories.objects.all()
+
     context={
         'title' : 'Home - Главная',
-        'content' : 'Магазин мебели HOME'
+        'content' : 'Магазин мебели HOME',
+        'categories' : categories
     }
     return render(request, 'main/about.html', context)
 
