b5c2985736219bf7ff7a5669cecc845ad430b2d9
Dmitry <dmitry@email.com>
2024-02-24 16:31:09 +0300

    Настроена проверка пользователя при регистрации

--- users/views.py
+++ users/views.py
@@ -5,11 +5,38 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from users.forms import UserLoginForm
+
 
 
 def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
+            if user:
+                auth.login(request, user)
+                messages.success(request, f"{username}, Вы вошли в аккаунт")
+
+                #if session_key:
+                #    Cart.objects.filter(session_key=session_key).update(user=user)
+
+                redirect_page = request.POST.get('next', None)
+                if redirect_page and redirect_page != reverse('user:logout'):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                    
+                return HttpResponseRedirect(reverse('main:index'))
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Home - Авторизация'
+        'title': 'Home - Авторизация',
+        'form': form
     }
     return render(request, 'users/login.html', context)
 
