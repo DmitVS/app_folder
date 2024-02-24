c1c930fa6d0e41ab94ef1a72eea6939ccaf489ea
Dmitry <dmitry@email.com>
2024-02-24 20:32:11 +0300

    Настроен ЛК (Profile) и Notification

--- users/views.py
+++ users/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
-from users.forms import UserLoginForm
+from users.forms import  ProfileForm, UserLoginForm, UserRegistrationForm
 
 
 
@@ -42,15 +42,39 @@ def login(request):
 
 
 def registration(request):
+    if request.method == 'POST':
+        form = UserRegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.instance
+            auth.login(request, user)
+            messages.success(request, f"{user.username}, Вы вошли в аккаунт")
+            return HttpResponseRedirect(reverse('main:index'))
+        
+    else:
+        form = UserLoginForm()
+
     context = {
-        'title': 'Home - Регистрация'
+        'title': 'Home - Регистрация',
+        'form': form
     }
     return render(request, 'users/registration.html', context)
 
 
+@login_required
 def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(data=request.POST, instance=request.user, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Профайл успешно обновлен")
+            return HttpResponseRedirect(reverse('user:profile'))
+    else:
+        form = ProfileForm(instance=request.user)
+
     context = {
         'title': 'Home - Кабинет',
+        'form': form,
     }
     return render(request, 'users/profile.html', context)
 
@@ -58,6 +82,7 @@ def users_cart(request):
     return render(request, 'users/users_cart.html')
 
 
+@login_required
 def logout(request):
     messages.success(request, f"{request.user.username}, Вы вышли из аккаунта")
     auth.logout(request)
